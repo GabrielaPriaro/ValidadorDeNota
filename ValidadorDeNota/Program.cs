f415d0bed8ae3474ffe3994d58fd002e83f1ce4c
Gabriela <gabriela.priaro@gmail.com>
2024-10-28 14:26:47 +0000

    Adicionar arquivos de projeto.

---
+++ ValidadorDeNota/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ValidadorDeNota
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
